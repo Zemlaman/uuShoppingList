06e3e0023595bafab5eadcc7f65008c4fb56fb8e
Zemlaman <matej.zemla@seznam.cz>
2023-12-10 22:42:12 +0100

    test

--- uuShoppinglistBE/tests/GetListTest.js
+++ uuShoppinglistBE/tests/GetListTest.js
@@ -2,12 +2,12 @@ const GetListAbl = require('../abl/shoppingList-abl/getList-abl');
 const assert = require('assert');
 
 describe('GetListAbl', function() {
-    it('should return a shopping list by id (happy day scenario)', async function() {
-        let res = { json: function(data) { this.data = data; return this; }, status: function(statusCode) { this.statusCode = statusCode; return this; }};
-        await GetListAbl({ params: { id: '65639c13588c34bec5e20e7d' } }, res);
-        assert.equal(res.statusCode, 200);
-        assert.equal(typeof res.data, 'object');
-    });
+  it('should return a shopping list by id (happy day scenario)', async function() {
+      let res = { json: function(data) { this.data = data; return this; }, status: function(statusCode) { this.statusCode = statusCode; return this; }};
+      await GetListAbl({ params: { id: '65639c13588c34bec5e20e7c' } }, res);
+      assert.equal(res.statusCode, 200);
+      assert.equal(typeof res.data, 'object');
+  });
 
     it('should return error if list does not exist', async function() {
         let res = { json: function(data) { this.data = data; return this; }, status: function(statusCode) { this.statusCode = statusCode; return this; }};
